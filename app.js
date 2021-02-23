210ee9351ff1eae31bd7477d2f809a753c5f5ddc
davisstacked <brettzvi@gmail.com>
2021-02-23 12:45:52 -0800

    oop vs functional programming

---
+++ app.js
@@ -0,0 +1,20 @@
+// Data Structures
+class Emoji {
+  constructor(param, param2, param3) {
+    super();
+    this.param = param;
+    this.param2 = param2; 
+  }  
+  
+  static method() {
+    return 0;
+  }
+}
+
+class SuperHuman extends Emoji {
+  constructor (){
+    (super);
+    this.property = 0;
+    
+  }
+}
